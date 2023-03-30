e2d7025d13f81a948658d4b97af258e355f87ba0
kilimanki <www.slipperstrong@gmail.com>
2023-03-30 11:34:54 +0200

    gg

--- src/components/Lessons/Lessons.jsx
+++ src/components/Lessons/Lessons.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
 import { getSubject } from 'redux/teacher-slice';
 import css from './Lessons.module.css';
 
